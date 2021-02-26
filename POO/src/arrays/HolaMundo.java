6f16b66bafa8ea193b5c6f174ad125c4024f432b
alumnoa <alumnoa@profesor-B85M-DS3H-A>
2021-02-26 13:19:07 +0100

    nuevo hola mundo, triplicado

---
+++ POO/src/arrays/HolaMundo.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class HolaMundo 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		
+	}
+
+}
